66c52aa2a13817aecf09a78dccaf48c8ac8b99bf
NguyenDucAo <aonguyen113@gmail.com>
2019-06-17 03:16:37 +0700

    Updates

---
+++ precache-manifest.1f32d1b6db7e464f52bcf2f26edff086.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "504e60e531c139837a1a",
+    "url": "/todo-app/static/js/main.504e60e5.chunk.js"
+  },
+  {
+    "revision": "4d0ac8b77d29b8e4d880",
+    "url": "/todo-app/static/js/1.4d0ac8b7.chunk.js"
+  },
+  {
+    "revision": "504e60e531c139837a1a",
+    "url": "/todo-app/static/css/main.c298f8eb.chunk.css"
+  },
+  {
+    "revision": "058208115d27226fb8917456239295aa",
+    "url": "/todo-app/index.html"
+  }
+];
